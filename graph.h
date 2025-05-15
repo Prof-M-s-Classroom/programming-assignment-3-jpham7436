e9419f7d6d247d0b6551ef60ea60c9c755def895
jpham7436 <jpham7436@sdsu.edu>
2025-05-15 16:31:01 -0700

    Update graph.h

--- graph.h
+++ graph.h
@@ -24,50 +24,6 @@ public:
     }
 
     void primMST() {
-  /*      int* key = new int[numVertices];      // Holds min edge weights
-        int* parent = new int[numVertices];   // Stores MST edges
-        bool* inMST = new bool[numVertices];  // Track vertices added to MST
-
-        MinHeap heap(numVertices);
-
-        for (int v = 0; v < numVertices; ++v) {
-            key[v] = INT_MAX;
-            parent[v] = -1;
-            inMST[v] = false;
-            heap.insert(v, key[v]);
-        }
-
-        // Start with vertex 0
-        key[0] = 0;
-        heap.decreaseKey(0, 0);
-
-        while (!heap.isEmpty()) {
-            int u = heap.extractMin();
-            inMST[u] = true;
-
-            // Check all neighbors
-            for (int v = 0; v < numVertices; ++v) {
-                int weight = adjMatrix[u][v];
-                if (weight != 0 && !inMST[v] && weight < key[v]) {
-                    key[v] = weight;
-                    parent[v] = u;
-                    heap.decreaseKey(v, weight);
-                }
-            }
-        }
-
-        // Print MST
-        int totalWeight = 0;
-        std::cout << "Edge   Weight\n";
-        for (int i = 1; i < numVertices; ++i) {
-            std::cout << parent[i] << " - " << i << "    " << adjMatrix[i][parent[i]] << "\n";
-            totalWeight += adjMatrix[i][parent[i]];
-        }
-        std::cout << "Total weight of MST: " << totalWeight << "\n";
-
-        delete[] key;
-        delete[] parent;
-        delete[] inMST; */
         int* weightArray = new int[numVertices];
         int* parent = new int[numVertices];
         bool* inMST = new bool[numVertices];
@@ -77,12 +33,12 @@ public:
             weightArray[i] = INT_MAX;
             parent[i] = -1;
             inMST[i] = false;
+            heap.insert(i, weightArray[i]); 
         }
 
-        // Start with vertex 0
-        heap.insert(0, 0);
+        heap.decreaseKey(0, 0); 
         weightArray[0] = 0;
-        parent[0] = -1;
+
 
         while (!heap.isEmpty()) {
             int vertex = heap.extractMin();
@@ -92,9 +48,9 @@ public:
                 int weight = adjMatrix[vertex][i];
 
                 if (weight != 0 && !inMST[i] && weight < weightArray[i]) {
-                    heap.insert(i, weight);
                     parent[i] = vertex;
                     weightArray[i] = weight;
+                    heap.decreaseKey(i, weight);  // ✅ use decreaseKey here
                 }
             }
         }
@@ -108,6 +64,9 @@ public:
         }
         cout << "Final weight is: " << totalWeight;
 
+        delete[] weightArray;
+        delete[] parent;
+        delete[] inMST;
     }
 
 
